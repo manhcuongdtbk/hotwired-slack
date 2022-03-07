baf922a7aa34e4c438bceb5b6b95b1b9fb740d10
manhcuongdtbk <manhcuongdtbk@gmail.com>
2022-03-07 20:29:14 +0700

    Add unread message count next to channel name when user got mention

--- app/javascript/controllers/unreads_controller.js
+++ app/javascript/controllers/unreads_controller.js
@@ -2,11 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
 
 export default class extends Controller {
+  static targets = ["mentions"]
+
   static values = {
     id: Number
   }
 
   connect() {
+    this.username = document.querySelector("meta[name='username']").getAttribute("content")
     this.subscription = consumer.subscriptions.create({ channel: "UnreadsChannel", id: this.idValue }, {
       connected: this._connected.bind(this),
       disconnected: this._disconnected.bind(this),
@@ -24,7 +27,41 @@ export default class extends Controller {
   _disconnected() {
   }
 
-  _received() {
+  _received(data) {
     this.element.classList.add("fw-bold")
+
+    if (data.mentions && data.mentions.includes(this.username)) {
+      let count = parseInt(this.mentionsTarget.textContent)
+      this.mentionsTarget.textContent = count + 1
+      this.mentionsTarget.removeAttribute("hidden")
+      this.notify(data.body)
+    }
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Notification/permission#examples
+  notify(message) {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      console.log("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      const notification = new Notification(message);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied" || Notification.permission === "default") {
+      Notification.requestPermission(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          const notification = new Notification(message);
+        }
+      });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
   }
 }
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,22 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
+
+  after_create :process_mentions
+
+  private
+
+  def process_mentions
+    UnreadsChannel.broadcast_to(channel, { mentions: mentioned_names, body: })
+  end
+
+  def mentioned_users
+    # Users other than the one who created this message
+    User.where(name: mentioned_names) - [user]
+  end
+
+  def mentioned_names
+    body.scan(/@([\w-]+)/) # [[name1], [name2]]
+        .flatten # [name1, name2]
+  end
 end
