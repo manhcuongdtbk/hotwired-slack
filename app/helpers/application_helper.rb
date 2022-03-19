63ec413a9a3e10a28b2b9f2bb4afc32ca38f7789
manhcuongdtbk <manhcuongdtbk@gmail.com>
2022-03-19 16:09:52 +0700

    Add flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  BOOTSTRAP_FLASH_CLASS_MAPPING = {
+    'success' => 'alert-success',
+    'error' => 'alert-danger',
+    'alert' => 'alert-warning',
+    'notice' => 'alert-info'
+  }.freeze
+
+  def flash_class_for(message_type)
+    stringified_message_type = message_type.to_s
+    BOOTSTRAP_FLASH_CLASS_MAPPING[stringified_message_type] || stringified_message_type
+  end
 end
